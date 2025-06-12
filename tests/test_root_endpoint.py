146ca36f865295db1a62d857c86bc33c7580ef22
Shahadat Hossain <sht.kcn@gmail.com>
2025-06-12 23:59:49 +0600

    adding pytest(3)

--- tests/test_root_endpoint.py
+++ tests/test_root_endpoint.py
@@ -1,5 +1,10 @@
 from rest_framework.test import APIClient
+import os
+import django
 
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "neeraloy_backend.settings")
+django.setup()
 
 def test_root_returns_200():
     client = APIClient()
