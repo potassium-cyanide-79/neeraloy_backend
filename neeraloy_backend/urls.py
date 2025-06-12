7ae1af78938caa64cb8474c3d20270fb72b81933
Shahadat Hossain <sht.kcn@gmail.com>
2025-06-12 23:50:52 +0600

    adding pytest(2)

--- neeraloy_backend/urls.py
+++ neeraloy_backend/urls.py
@@ -18,8 +18,10 @@ from django.contrib import admin
 from django.urls import path
 from django.http import JsonResponse
 
+
 def root_view(request):
     return JsonResponse({"message": "API Root Working"})
+
 
 urlpatterns = [
     path('', root_view),
